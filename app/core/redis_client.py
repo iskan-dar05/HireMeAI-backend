be8ed8129dba4432d4a26d278f1a197a05468bf3
iskandar boukhenoufa <iskandar@iskandar.iskandar@skou.com>
2025-12-29 19:24:41 -0500

    fix redis

--- app/core/redis_client.py
+++ app/core/redis_client.py
@@ -1,10 +1,17 @@
 import redis
-from .config import settings
 
-
+# Use your RedisLabs / Upstash credentials
 redis_client = redis.Redis(
-	host="redis",
-	port=6379,
-	db=0,
-	decode_responses=True
+    host='redis-11253.c283.us-east-1-4.ec2.cloud.redislabs.com',
+    port=11253,
+    decode_responses=True,
+    username="default",   # optional, depends on provider
+    password="ClfnhpP6j2KoC3kMHTMbIQwKhEwESRTE",
 )
+
+# Test connection
+success = redis_client.set('foo', 'bar')
+print("SET success:", success)
+
+result = redis_client.get('foo')
+print("GET result:", result)
